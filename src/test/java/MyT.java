729db82b3f4ee9023c2899ab29466d5e796cacb4
sherrychen55 <leileiomi@126.com>
2021-05-30 14:14:08 +0800

    test

---
+++ src/test/java/MyT.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MyT {
+    @Test
+    public void demo() throws MalformedURLException {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        //chrome web driver已经放在PATH路径中
+        WebDriver driver = new RemoteWebDriver(new URL("http://192.168.209.1:4444/wd/hub/"), chromeOptions);
+        driver.get("http://www.baidu.com");
+        WebElement inputElement = driver.findElement(By.id("kw"));
+        inputElement.sendKeys("'test");
+        inputElement.submit();//回车
+        driver.quit();
+        //test
+    }
+}
